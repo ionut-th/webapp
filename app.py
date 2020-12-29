dad6dc2e6949ac7da34d7cf6948078623772fb88
ionut-th <ionut-tudor@hotmail.com>
2020-12-29 14:55:58 +0200

    First commit
    Base files.

---
+++ app.py
@@ -0,0 +1,198 @@
+import os
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import *
+from sqlalchemy import create_engine
+
+
+from helpers import apology, login_required, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = create_engine("sqlite:///finance.db", echo=True)
+
+# Make sure API key is set
+#if not os.environ.get("API_KEY"):
+ #   raise RuntimeError("API_KEY not set")
+
+
+@app.route("/")
+@login_required
+def index():
+    """Show portfolio of stocks"""
+
+
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username").lower())
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # Forget any user_id
+    session.clear()
+
+     # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password")
+
+
+
+        # get input data
+        user = request.form.get("username").lower()
+        password = request.form.get("password").lower()
+        confirmation = request.form.get("confirmation").lower()
+        # check for valid input
+        if len(user) < 3:
+            return apology("user too short", 403)
+        if len(password) <3:
+            return apology("password too short")
+        if password != confirmation:
+            return apology("password and retyped passwords do not match!")
+        # Check if user exists
+        result = db.execute("SELECT * FROM users WHERE username = %s", user)
+        if len(result) > 0:
+            return apology("user already exists", 403)
+        #generate pass hash
+        passHash = generate_password_hash(password, "sha256")
+        # Insert Query into DB
+        db.execute(f"INSERT INTO users (username, hash) VALUES (?)", (user, passHash))
+        # Redirect user to home page
+        return redirect("/login")
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+
+
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
+
+
+@app.route("/changepass", methods=["GET", "POST"])
+@login_required
+def changepass():
+
+    if request.method == "POST":
+
+        if not request.form.get("currentPass"):
+            return render_template("changepass.html", msg="Current password wrong")
+
+
+        if not request.form.get("newPass"):
+            return render_template("changepass.html", msg="You did not enter a new password")
+        if not request.form.get("confirmation"):
+            return render_template("changepass.html", msg="Please retype your new password again correctly")
+
+        curPass = request.form.get("currentPass")
+        newPass = request.form.get("newPass")
+        confirm = request.form.get("confirmation")
+
+        if newPass != confirm:
+            return render_template("changepass.html", msg="New password does not match, please retype your password confirmation")
+
+        passHash = generate_password_hash(newPass, "sha256")
+        dbHash = db.execute("SELECT hash FROM users WHERE id = ?", session['user_id'])
+
+        check = check_password_hash( dbHash[0]['hash'], curPass)
+
+        if not check:
+            return render_template("changepass.html", msg="Current password wrong")
+        else:
+            query = "UPDATE users SET hash = ? WHERE users.id = ?"
+            db.execute(query, passHash, session['user_id'])
+            return render_template("changepass.html", msg="Password changed successfully")
+
+
+    return render_template("changepass.html")
+
